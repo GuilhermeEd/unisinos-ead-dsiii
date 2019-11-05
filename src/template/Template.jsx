cc02127eca30e4f6de921822a713501c2c627a2e
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2019-11-04 23:28:07 -0500

    Add template header

--- src/template/Template.jsx
+++ src/template/Template.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Menu } from 'antd';
+import { Link, withRouter } from 'react-router-dom';
 
-const Template = ({ children }) => {
-  return <>{children}</>;
+import * as Styled from './styles/Template.styles';
+
+const Template = ({ children, match }) => {
+  const selectedKeys = [match.path.substr(1)];
+
+  return (
+    <Styled.Container>
+      <Styled.Header>
+        <Menu mode="horizontal" selectedKeys={selectedKeys}>
+          <Menu.Item key="new-user">
+            <Link to="/new-user">Criar Usuário</Link>
+          </Menu.Item>
+          <Menu.Item key="new-project">
+            <Link to="/new-project">Criar Projeto</Link>
+          </Menu.Item>
+          <Menu.Item key="donate">
+            <Link to="/donate">Doar</Link>
+          </Menu.Item>
+        </Menu>
+      </Styled.Header>
+      <Styled.Content>{children}</Styled.Content>
+    </Styled.Container>
+  );
 };
 
 Template.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  match: PropTypes.object.isRequired
 };
 
-export default Template;
+export default withRouter(Template);
---
+++ src/template/styles/Template.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Header = styled.header`
+  width: 100%;
+`;
+
+export const Content = styled.section`
+  flex: 1;
+`;
